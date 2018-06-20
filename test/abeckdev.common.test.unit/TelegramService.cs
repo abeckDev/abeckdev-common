4cae4dfed3c4e6cbf81cf86def7e68e2914ec52a
Alexander Beck <alexander.beck@t-systems.com>
2018-06-20 23:58:02 +0200

    Added Test Project

---
+++ test/abeckdev.common.test.unit/TelegramService.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using abeckdev.common.Services;
+
+namespace abeckdev.common.test.unit
+{
+    public class TelegramService
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
